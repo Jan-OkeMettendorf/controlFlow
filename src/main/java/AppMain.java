f647b164734c9bfbbc643f3fad67396785ee4336
Jan-Oke Mettendorf <jan-oke-mettendorf@web.de>
2021-09-09 14:56:21 +0200

    add new code

--- src/main/java/AppMain.java
+++ src/main/java/AppMain.java
@@ -2,67 +2,7 @@ public class AppMain {
 
     public static void main(String[] args) {
 
-        int number = 60;
-        String Alarm = "green";
-
-        System.out.println(checkAlarmSignalAndCapacity(number, Alarm));
-
-    }
-
-
-    public static String checkAlarmSignalAndCapacity(int capacity, String alarmSignal){
-
-        switch (alarmSignal){
-
-            case "red":
-
-                if (capacity > 0) {
-                    return "maximum number reached.";
-                }
-                    return "maximum number not reached.";
-
-            case "yellow":
-
-                return checkCapacityToAlarmsignal(30, capacity);
-
-            case "green":
-
-                    return checkCapacityToAlarmsignal(60, capacity);
-
-            default:
-
-                    return "unknown Alarm.";
-
-        }
 
 
     }
-    public static String checkCountGreaterThan30(int capacity){
-
-        if (capacity > 30) {
-
-            return "maximum number reached.";
-
-        } else if (capacity == 30) {
-
-            return "maximum number reached.";
-
-        } else {
-
-            return "maximum number not reached.";
-        }
-
-    }
-
-    public static String checkCapacityToAlarmsignal(int allowedCapacity, int actualCapacity){
-
-        if (actualCapacity > allowedCapacity) {
-
-            return "maximum number reached.";
-
-        }
-
-        return "maximum number not reached.";
-    }
-
 }
--- src/test/java/AppMainTest.java
+++ src/test/java/AppMainTest.java
@@ -1,185 +1,25 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.Test;
 
 public class AppMainTest {
 
-    @Test
-    void checkAlarmSignalAndCapacityRedCapacityAllowed(){
+   @Test
+    void testCalculateFac(){
 
-        //GIVEN
+       //GIVEN
 
-            int a = 0;
-            String Alarm = "red";
+       int a = 2;
 
-        //WHEN
+       //WHEN
 
-            String actual = AppMain.checkAlarmSignalAndCapacity(a, Alarm);
+       int actual = AppMain.calculateFac(a);
 
-        //THEN
+       //THEN
 
-            Assertions.assertEquals("maximum number not reached.", actual);
+       Assertions.assertEquals(0, actual);
 
-    }
-
-    @Test
-    void checkAlarmSignalAndCapacityRedCapacityNotAllowed(){
-
-        //GIVEN
-
-            int a = 1;
-            String Alarm = "red";
-
-        //WHEN
-
-            String actual = AppMain.checkAlarmSignalAndCapacity(a, Alarm);
-
-        //THEN
-
-            Assertions.assertEquals("maximum number reached.", actual);
-
-    }
-
-    @Test
-    void checkAlarmSignalAndCapacityYellowCapacityAllowed(){
-
-        //GIVEN
-
-            int a = 30;
-            String Alarm = "yellow";
-
-        //WHEN
-
-            String actual = AppMain.checkAlarmSignalAndCapacity(a, Alarm);
-
-        //THEN
-
-            Assertions.assertEquals("maximum number not reached.", actual);
-
-    }
-
-    @Test
-    void checkAlarmSignalAndCapacityYellowCapacityNotAllowed(){
-
-        //GIVEN
-
-            int a = 31;
-            String Alarm = "yellow";
-
-        //WHEN
-
-            String actual = AppMain.checkAlarmSignalAndCapacity(a, Alarm);
-
-        //THEN
-
-            Assertions.assertEquals("maximum number reached.", actual);
-
-    }
-
-    @Test
-    void checkAlarmSignalAndCapacityGreenCapacityAllowed(){
-
-        //GIVEN
-
-            int a = 60;
-            String Alarm = "yellow";
-
-        //WHEN
-
-            String actual = AppMain.checkAlarmSignalAndCapacity(a, Alarm);
-
-        //THEN
-
-            Assertions.assertEquals("maximum number not reached.", actual);
-
-    }
-
-    @Test
-    void checkAlarmSignalAndCapacityGreenCapacityNotAllowed(){
-
-        //GIVEN
-
-            int a = 61;
-            String Alarm = "yellow";
-
-        //WHEN
-
-            String actual = AppMain.checkAlarmSignalAndCapacity(a, Alarm);
-
-        //THEN
-
-            Assertions.assertEquals("maximum number reached.", actual);
-
-    }
-
-    @Test
-    void checkAlarmSignalAndCapacityUnknownAlarm(){
-
-        //GIVEN
-
-            int a = 61;
-            String Alarm = "";
-
-        //WHEN
-
-            String actual = AppMain.checkAlarmSignalAndCapacity(a, Alarm);
-
-        //THEN
-
-            Assertions.assertEquals("unknown Alarm.", actual);
-
-    }
-
-
-    /*@Test
-    void testCheckCountGreaterThan30True(){
-
-        //GIVE
-
-            int a = 34;
-
-        //WHEN
-
-            String result = AppMain.checkCountGreaterThan30(a);
-
-        //THEN
-
-        Assertions.assertEquals("maximum number reached", result);
-
-    }
-
-    @Test
-    void testCheckCountGreaterThan30False(){
-
-        //GIVE
-
-            int a = 15;
-
-        //WHEN
-
-            String result = AppMain.checkCountGreaterThan30(a);
-
-        //THEN
-
-            Assertions.assertEquals("maximum number not reached", result);
-
-    }
-
-    @Test
-    void testCheckCountIs30(){
-
-        //GIVE
-
-            int a = 30;
-
-        //WHEN
-
-            String result = AppMain.checkCountGreaterThan30(a);
-
-        //THEN
-
-            Assertions.assertEquals("maximum number reached", result);
-
-    }*/
+   }
 
 
 }
