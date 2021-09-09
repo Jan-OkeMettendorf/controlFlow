2e0e419651b470e3d2fee9573f23cd8073ae6582
Jan-Oke Mettendorf <jan-oke-mettendorf@web.de>
2021-09-09 16:35:40 +0200

    add code

--- src/main/java/AppMain.java
+++ src/main/java/AppMain.java
@@ -2,29 +2,9 @@ public class AppMain {
 
     public static void main(String[] args) {
 
-            int number = 5;
-
-
-        System.out.println(calculateFac(number));
-
     }
 
-    public static int calculateFac(int value){
 
-        int fac = 1;
-        int i = 1;
-
-        if (value == 0) {
-            return fac;
-        }
-
-
-        while( i <= value){
-            fac *= i;
-            i++;
-        }
-
-        return fac;
 
     }
 }
--- src/test/java/AppMainTest.java
+++ src/test/java/AppMainTest.java
@@ -6,26 +6,6 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 public class AppMainTest {
 
-   @ParameterizedTest
-   @CsvSource({
-
-           "0, 1",
-           "1, 1",
-           "2, 2",
-           "3, 6",
-           "4, 24",
-           "5, 120",
-
-           })
-    public void testCalculateFac(int given, int expected){
-
-       //WHEN
-
-       int actual = AppMain.calculateFac(given);
-
-       //THEN
-
-       Assertions.assertEquals(expected, actual);
 
    }
 
